142fa8b691fa3450a3af54a944453a7499400c89
PierreThinnes <thinnes.kuso@gmail.com>
2024-05-16 01:34:29 +0200

    Version valide du Login

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose")
 
-const bookRoutes = require("./routes/book");
+//const bookRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
 
 const path = require('path');
@@ -10,21 +10,24 @@ require("dotenv").config()
 
 
 const connexionMongoose = () => {
-    try {
-        mongoose.connect(process.env.DBLINK, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+    mongoose.connect(process.env.DBLINK, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
         console.log("Connexion à MongoDB réussie !");
-    } catch {
-        (err) => console.log("Connexion à MongoDB échouée !", err);
-    }
+    })
+    .catch((err) => {
+        console.error("Connexion à MongoDB échouée !", err);
+    });
 };
 connexionMongoose();
 
 //Création de l'application
 const app = express();
 
+//Middleware permettant à Express d'extraire le corps Json des reaquetes POST
+app.use(express.json());
 
 // Middleware gérant les erreurs de CORS
 app.use((req, res, next) => {
@@ -36,14 +39,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-//Middleware permettant à Express d'extraire le corps Json des reaquetes POST
-app.use(express.json());
 
 // mise avant pour indiquer à Express qu'il faut gérer la ressource images de manière statique
 app.use("/images", express.static(path.join(__dirname, "images"))); 
 
 // Enregistrement des routeurs
-app.use("/api/books", bookRoutes);
+//app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
 
 
