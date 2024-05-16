050a2e2de92ae9c62281bd49785319407c13e97c
PierreThinnes <thinnes.kuso@gmail.com>
2024-05-16 11:57:07 +0200

    Ajout des fonctionnalitées Books (publier,modifier,supprimer)/Ajout du systeme de notation/Regroupement de Multer&Sharp dans upload.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose")
 
-//const bookRoutes = require("./routes/book");
+const bookRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
 
 const path = require('path');
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 app.use("/images", express.static(path.join(__dirname, "images"))); 
 
 // Enregistrement des routeurs
-//app.use("/api/books", bookRoutes);
+app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
 
 
